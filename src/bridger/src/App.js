110f95cf825f05ace0cebd32d336c9bd89b11296
Yan Digilov <yan@pandi.fi>
2021-12-27 12:47:01 -0600

    added react app to src

---
+++ src/bridger/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div>
+      Display MTC balance ETH:
+      BOX INPUT
+      Transfer
+
+      Display MTC balance BSC
+      BOX INPUT
+      Transfer
+    </div>
+  );
+}
+
+export default App;
